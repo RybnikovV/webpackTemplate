be6901717089deb1136f85df19356fc06eee943a
Rybnikov.VV <va6d6@mail.ru>
2021-09-23 16:28:12 +0500

    V-1.1.2 fix enter point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './stylus/main';
 import json from './assets/json';
 
-const post = new Post('Webpack Post Title');
-
-console.log('Post to string:', post.toString());
-
 console.log('show json file', json);
